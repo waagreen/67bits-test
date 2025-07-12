86efad965b2724e7d237b8d54f0a4e0e615de772
Wagner Costa <silvacostawagner@gmail.com>
2025-07-12 16:41:26 -0300

    game events system
    System for handling game events with custom properties. The idea is to facilitate communication between systems without coupling them.

---
+++ Assets/Scripts/Utility/GameEvents.cs
@@ -0,0 +1,9 @@
+// Class to register all game events
+
+public class GameEvent { }
+
+public class OnPause : GameEvent { }
+
+public class OnCollect : GameEvent { }
+
+public class OnLevelUp : GameEvent { }
