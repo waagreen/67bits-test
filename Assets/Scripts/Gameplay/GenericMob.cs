ab8e1206654e200cb7ed806cab8a3f5271a67dbd
Wagner Costa <silvacostawagner@gmail.com>
2025-07-14 14:45:09 -0300

    Improving mobs movement
    Making mobs appear more 'alive' using random values for idle and movement times

--- Assets/Scripts/Gameplay/GenericMob.cs
+++ Assets/Scripts/Gameplay/GenericMob.cs
@@ -3,33 +3,35 @@ using UnityEngine;
 
 public class GenericMob : CharacterMovement
 {
+    [Header("Mob settings")]
     [SerializeField] private List<GameObject> skins;
-    [Header("Movement options")]
-    [SerializeField][Min(0f)] private float timeToChangeDirection = 1f;
+    [SerializeField][Min(0f)] private float minMoveTime = 2f, maxMoveTime = 4f, minIdleTime = 1f, maxIdleTime = 2f;
 
     private GameObject activeSkin = null;
 
-    private float timer = 0f;
+    private float movementDelta = 0f, idleDelta = 0f;
+    private float movementTime = 0f, idleTime = 0f;
     private Vector2 lastDirection = default;
 
-    public override Vector2 MovementInput
+    public override Vector2 MovementInput => GetDirection();
+
+    private void OnValidate()
     {
-        get
-        {
-            timer += Time.deltaTime;
-            if (timer >= timeToChangeDirection)
-            {
-                lastDirection = new(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-                timer = 0f;
-            }
-            return lastDirection.normalized;
-        }
+        maxIdleTime = Mathf.Max(minIdleTime, maxIdleTime);
+        minIdleTime = Mathf.Min(minIdleTime, maxIdleTime);
+
+        maxMoveTime = Mathf.Max(minMoveTime, maxMoveTime);
+        minMoveTime = Mathf.Min(minMoveTime, maxMoveTime);
     }
 
     protected override void Start()
     {
         base.Start();
+        OnValidate();
+
+        // Choose a random direction and a amount of time to move
         lastDirection = new(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        movementTime = Random.Range(minMoveTime, maxMoveTime);
     }
 
     private void OnEnable()
@@ -42,4 +44,27 @@ public class GenericMob : CharacterMovement
     {
         if (activeSkin != null) activeSkin.SetActive(false);
     }
+
+    // Mobs start moving in a random direction for the given movement time
+    // After they choose another random direction and wait for the idle time
+    private Vector2 GetDirection()
+    {
+        float delta = Time.deltaTime;
+
+        idleDelta += delta;
+        if (idleDelta < idleTime) return Vector2.zero;
+
+        movementDelta += delta;
+        if (movementDelta >= movementTime)
+        {
+            lastDirection = new(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+
+            idleTime = Random.Range(minIdleTime, maxIdleTime);
+            movementTime = Random.Range(minMoveTime, maxMoveTime);
+
+            movementDelta = 0f;
+            idleDelta = 0f;
+        }
+        return lastDirection.normalized;
+    } 
 }
