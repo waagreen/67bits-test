6db49dfca7d7a55b96ff2f6100f17e8fd95d17b5
Wagner Costa <silvacostawagner@gmail.com>
2025-07-15 14:57:47 -0300

    removed rotation and noise from pile
    In the end, those features were only a detriment for the visuals, with only smooth position update the pile inertia effect is much more performant e clean.

--- Assets/Scripts/Utility/HandleRagdoll.cs
+++ Assets/Scripts/Utility/HandleRagdoll.cs
@@ -9,21 +9,17 @@ public class HandleRagdoll : MonoBehaviour
     [SerializeField] private bool enableOnAwake = false;
     [SerializeField] private LayerMask groundedBlackList = 0, carriedBlackList = 0;
 
-    private float followDelay, perlinSeed, swayAmplitude, swayFrequency;
-    private Vector3 smoothedVelocity, smoothedAngularVelocity;
+    private float followDelay;
     private Vector3 localOffset;
-    private Quaternion localRotation;
     private Rigidbody[] bodies;
     private Transform target = null;
     // Using constants because we are dealing with small numbers. Other values gives worse results.
     private const float kAmplitudeIncrease = 0.15f, kFrequencyIncrease = 0.1f, kStackPower = 1.2f;
+    private Vector3 smoothedPosition, velocityRef;
 
     private void Awake()
     {
         bodies = GetComponentsInChildren<Rigidbody>();
-
-        // This seed makes sure every body has a different motion
-        perlinSeed = Random.Range(0f, 1000f);
 
         for (int i = 0; i < bodies.Length; i++)
         {
@@ -37,47 +33,28 @@ public class HandleRagdoll : MonoBehaviour
     {
         if (target == null) return;
 
-        // Position and rotation relative to target
+        // Position relative to target
         Vector3 worldTargetPos = target.TransformPoint(localOffset);
-        Quaternion worldTargetRot = target.rotation * localRotation;
-        worldTargetPos += GetPerlinSway();
 
-        Vector3 delta = worldTargetPos - anchorBone.position;
-        Quaternion deltaRot = worldTargetRot * Quaternion.Inverse(anchorBone.rotation);
+        // Calculating position with smooth damp to prevent movement overshoot
+        smoothedPosition = Vector3.SmoothDamp
+        (
+            smoothedPosition,
+            worldTargetPos,
+            ref velocityRef,
+            followDelay,
+            Mathf.Infinity,
+            Time.fixedDeltaTime
+        );
 
-        Vector3 targetVelocity = delta / Time.fixedDeltaTime;
-
-        // Convert delta rotation to angular velocity
-        deltaRot.ToAngleAxis(out float angle, out Vector3 axis);
-        if (angle > 180f) angle -= 360f;
-
-        Vector3 targetAngularVelocity = angle * Mathf.Deg2Rad * axis.normalized / Time.fixedDeltaTime;
-        targetAngularVelocity = (axis == Vector3.zero) ? Vector3.zero : targetAngularVelocity;
-
-        // Introducing "inertia" by adding delay while updating upper bodies position and rotation
-        float lerpFactor = Mathf.Clamp01(Time.fixedDeltaTime / followDelay);
-        smoothedVelocity = Vector3.Lerp(smoothedVelocity, targetVelocity, lerpFactor);
-        smoothedAngularVelocity = Vector3.Lerp(smoothedAngularVelocity, targetAngularVelocity, lerpFactor);
+        // New velocity is the diference in position between target and body over physics step delta
+        Vector3 linearVel = (smoothedPosition - anchorBone.position) / Time.fixedDeltaTime;
 
         foreach (var rb in bodies)
         {
             if (rb.isKinematic) continue; // skip if kinematic (safety check)
-
-            rb.linearVelocity = smoothedVelocity;
-            rb.angularVelocity = smoothedAngularVelocity;
+            rb.linearVelocity = linearVel;
         }
-    }
-
-    private Vector3 GetPerlinSway()
-    {
-        // Perlin noise oscilation
-        float time = Time.time * swayFrequency;
-        float noiseX = (Mathf.PerlinNoise(perlinSeed, time) - 0.5f) * 2f;
-        float noiseZ = (Mathf.PerlinNoise(perlinSeed + 100f, time) - 0.5f) * 2f;
-
-        // Sway offset project on the XZ plane relative to target position
-        Vector3 sway = new Vector3(noiseX, 0f, noiseZ) * swayAmplitude;
-        return target.TransformDirection(sway);
     }
 
     public void SetRagdollKinematic(bool state)
@@ -88,21 +65,21 @@ public class HandleRagdoll : MonoBehaviour
         }
     }
 
-    public void SetFollowDelay(float followDelay, int stackIndex, float swayFrequency, float swayAmplitude)
+    public void SetFollowDelay(float followDelay, int stackIndex)
     {
         this.followDelay = Mathf.Max(0.01f, followDelay) * Mathf.Pow(stackIndex, kStackPower);
-        this.swayAmplitude = swayAmplitude * (1f + (stackIndex * kAmplitudeIncrease));
-        this.swayFrequency = swayFrequency * (1f + (stackIndex * kFrequencyIncrease));
     }
 
-
-    public void AttachToTarget(Transform target, Vector3 localOffset, Quaternion localRotation)
+    public void AttachToTarget(Transform target, Vector3 localOffset)
     {
         this.target = target;
         this.localOffset = localOffset;
-        this.localRotation = localRotation;
 
         foreach (var rb in bodies) rb.excludeLayers = carriedBlackList;
+
+        // Initializing position and velocity
+        smoothedPosition = anchorBone.position;
+        velocityRef = Vector3.zero;
     }
 
     public void AddImpulse(Vector3 force)
